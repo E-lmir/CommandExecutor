43744ebd5702f0d1a58fc050e3d088da9ddb2ab8
MollyERG <mollymaloun@gmail.com>
2024-03-30 17:59:43 +0300

    Добавьте файлы проекта.

---
+++ CommandExecutor/CommandExecutor.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Executor
+{
+    public class CommandExecutor
+    {
+        private ProcessStartInfo processInfo;
+        public CommandExecutor() => processInfo = new() { FileName = "cmd.exe", UseShellExecute = true };
+
+        public void Execute(params string[] args)
+        {        
+            var arguments = new StringBuilder();
+            arguments.Append("/C");
+            arguments.AppendJoin("&&", args);
+
+            processInfo.Arguments = arguments.ToString();
+            using var process = Process.Start(processInfo);
+        }
+    }
+}

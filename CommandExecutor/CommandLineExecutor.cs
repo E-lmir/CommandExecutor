cb186a3d5d7f667e670dca9b9f6b0675229a1a7a
MollyERG <mollymaloun@gmail.com>
2024-04-01 21:48:09 +0300

    Update xml documentation

--- CommandExecutor/CommandLineExecutor.cs
+++ CommandExecutor/CommandLineExecutor.cs
@@ -3,22 +3,29 @@ using System.Text;
 
 namespace CommandExecutor
 {
+    /// <summary>
+    /// Provides an easier way to execute comands.
+    /// </summary>
     public class CommandLineExecutor
     {
         private ProcessStartInfo processInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the  <see cref="CommandLineExecutor"/> class.
+        /// </summary>
         public CommandLineExecutor() => processInfo = new() { FileName = "cmd.exe", UseShellExecute = true };
 
 
         /// <summary>
-        /// Execute commands
-        /// </summary>
+        /// Execute commands.<br/>
         /// <example>
         /// This shows how to execute command.
         /// <code>
         ///     var executor = new CommandLineExecutor();
-        ///     executor.Execute("cd C:/", "rmdir Windows");
+        ///     executor.Execute("cd C:/", "rmdir windows");
         /// </code>
         /// </example>
+        /// </summary>
         public void Execute(params string[] args)
         {        
             var arguments = new StringBuilder();
